cdb6f14b95ef372b35fe64510b8fdbe9159d6d3c
Pooja-Lohar25 <poojalohar789@gmail.com>
2023-01-25 00:48:34 +0530

    post method

--- app/app.js
+++ app/app.js
@@ -1,12 +1,21 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
 
 app.use(express.static(path.resolve(__dirname,'../assets')) )
 // app.get('/',(req,res)=>{
 //     res.sendFile(path.resolve(__dirname,'../assets','index.html'))
 //     console.log('done')
 // })
+
+
+
+app.post('/log',(req,res)=>{
+    console.log(req.body)
+    res.send(req.body)
+})
 
 app.listen(4000,()=>{
     console.log("server listening on port 4000")
