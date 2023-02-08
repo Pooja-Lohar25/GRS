fe8cd9cdcf591782ad93347d62cf0120e3b01165
Pooja-Lohar25 <poojalohar789@gmail.com>
2023-02-08 21:23:47 +0530

    ..

--- app/app.js
+++ app/app.js
@@ -1,31 +1,39 @@
 const express = require('express')
 const app = express()
 const path = require('path')
+const session = require('express-session')
+const dotenv = require('dotenv')
 
 // const {sequelize} = require('../database/dbconnect')
+
 const controllers = require('../database/controllers')
+app.use(session({
+    secret: process.env.SECRET,
+    resave: true,
+    saveUninitialized: true
+}));
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true})) //parsing form data to access it in routes
 
+app.get('/',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'../assets','index.html'))
+    console.log(req.sessionID)
+})
 app.use(express.static(path.resolve(__dirname,'../assets')) )
-// app.get('/',(req,res)=>{
-//     res.sendFile(path.resolve(__dirname,'../assets','index.html'))
-//     console.log('done')
-// })
 
 
- app.post('/login', async (req,res)=>{
+app.post('/login', async (req,res)=>{
+    
     result = await controllers.login(req)
     if(result == true)
     {
-        
         res.status(200).sendFile(path.resolve(__dirname,'../assets','dashboard.html'))
     }
     else{
         res.status(401).sendFile(path.resolve(__dirname,'../assets','error.html'))
     }
- })
+})
 
 app.post('/signup',async (req,res)=>{
     result = await controllers.signup(req)
