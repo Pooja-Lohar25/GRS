15c25b4af3dab8d4838bbb5474474d5bd9e814f3
Rohit <iamrohit2112@gmail.com>
2023-02-26 23:37:55 +0530

    Added hide show functionality to admin dashboard

--- assets/faculty.js
+++ assets/faculty.js
@@ -42,3 +42,13 @@ function validatePassword(){
 
 password.onchange = validatePassword;
 confirm_password.onkeyup = validatePassword;
+
+const targetDiv = document.getElementById("target");
+const btn = document.getElementById("toggle");
+btn.onclick = function () {
+  if (targetDiv.style.display !== "none") {
+    targetDiv.style.display = "none";
+  } else {
+    targetDiv.style.display = "block";
+  }
+};
